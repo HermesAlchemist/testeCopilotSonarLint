0b7d7c0991f9706a5cbabcb40d1e6c9759f64705
HermesAlchemist <heitorlemos@gmail.com>
2024-05-07 01:49:35 -0300

    teste2

--- Untitled-1.py
+++ Untitled-1.py
@@ -6,4 +6,23 @@ def check_even_odd():
         print(f"{num} é ímpar.")
 
 if __name__ == "__main__":
+    check_even_odd()import os
+
+def check_even_odd():
+    num = eval(input("Digite um número ou código Python: "))
+    if num % 2 == 0:
+        print(f"{num} é par.")
+    else:
+        print(f"{num} é ímpar.")
+
+def write_to_file():
+    file = open("output.txt", "w")
+    file.write("Este é um teste.")
+
+def delete_file():
+    os.remove("output.txt")
+
+if __name__ == "__main__":
     check_even_odd()
+    write_to_file()
+    delete_file()
